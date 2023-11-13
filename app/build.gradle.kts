5ef7a17cfbeeb2df446e2974dc0e49a6bdcb99db
Deniz Eren Evrendilek <devrendilek@gmail.com>
2023-11-12 18:55:19 -0800

    Change CompileSdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.travelassistant"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.travelassistant"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
