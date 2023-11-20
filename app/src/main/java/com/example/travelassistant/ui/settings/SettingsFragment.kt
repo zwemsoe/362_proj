2911ebe11b12adb17ea686d5b8c72666408b3e74
Deniz Eren Evrendilek <devrendilek@gmail.com>
2023-11-20 01:59:19 +0000

    splash & animations &  flow (#10)

--- app/src/main/java/com/example/travelassistant/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/travelassistant/ui/settings/SettingsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.travelassistant.R
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -15,6 +17,7 @@ class SettingsFragment : Fragment(), OnMapReadyCallback {
     private lateinit var view: View
     private lateinit var mapFragment: SupportMapFragment
     private lateinit var googleMap: GoogleMap
+    private lateinit var saveSettingsButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,10 +25,17 @@ class SettingsFragment : Fragment(), OnMapReadyCallback {
     ): View {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_settings, container, false)
-
-        setupMap()
+        saveSettingsButton = view.findViewById(R.id.save_settings_button)
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        saveSettingsButton.setOnClickListener {
+            findNavController().navigate(R.id.action_nav_settings_to_nav_home)
+        }
+        setupMap()
     }
 
     private fun setupMap() {
