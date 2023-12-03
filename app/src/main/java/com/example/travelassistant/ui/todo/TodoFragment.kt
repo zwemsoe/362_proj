74bc93c9c3e6ad966b65f3f3a1c73b935ad878fd
Kenny Mim <kenny_mim@sfu.ca>
2023-12-03 02:25:26 -0800

    fixed todo compiling

--- app/src/main/java/com/example/travelassistant/ui/todo/TodoFragment.kt
+++ app/src/main/java/com/example/travelassistant/ui/todo/TodoFragment.kt
@@ -3,7 +3,9 @@ package com.example.travelassistant.ui.todo
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,15 +17,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.example.travelassistant.R
-import com.example.travelassistant.manager.DataStoreManager
 import com.example.travelassistant.models.user.TodoItem
 import com.example.travelassistant.models.user.User
 import com.example.travelassistant.models.user.UserRepository
 import com.example.travelassistant.viewModels.UserViewModel
 import com.example.travelassistant.viewModels.UserViewModelFactory
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import com.google.firebase.auth.FirebaseAuth
 import java.util.UUID
 
 
@@ -33,15 +32,21 @@ class TodoFragment : Fragment() {
     private lateinit var viewModel: TodoViewModel
 
     private lateinit var todoListView : RecyclerView
+    private lateinit var todoListAdapter : TodoRecyclerAdapter
     private var todoList : List<TodoItem> = listOf()
+    private lateinit var myUser : String
 
-    private lateinit var dataStoreManager: DataStoreManager
     private lateinit var userRepository: UserRepository
     private lateinit var userViewModel: UserViewModel
+    private lateinit var auth: FirebaseAuth
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        dataStoreManager = DataStoreManager(context)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -59,19 +64,21 @@ class TodoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         todoListView = view.findViewById(R.id.todo_created_container)
-        observeDataStoreChanges()
+        //observeDataStoreChanges()
 
+        userViewModel.getUser(auth.currentUser!!.uid)
         userViewModel.user.observe(viewLifecycleOwner) { user ->
             if (user == null) {
                 return@observe
             }
+            myUser = user.id
             setupButtons(user)
-            setupSuggestions(user)
             getUserTodoList(user)
         }
+        setupSuggestions()
     }
 
-    private fun setupSuggestions(user : User) {
+    private fun setupSuggestions() {
         val container = view.findViewById<LinearLayout>(R.id.todo_suggestions_container)
         val loadingOrFail = TextView(requireContext())
         loadingOrFail.text = "Loading..."
@@ -93,7 +100,7 @@ class TodoFragment : Fragment() {
                     val item = TodoItem(itemID,"", false)
 
                     item.task = suggestion
-                    userViewModel.addTodoItem(user.id,item)
+                    userViewModel.addTodoItem(myUser,item)
                     todoListView.adapter?.notifyItemInserted(todoList.size)
 
                     Toast.makeText(requireContext(), "Added suggestion!", Toast.LENGTH_SHORT).show()
@@ -107,40 +114,43 @@ class TodoFragment : Fragment() {
     //Add todoItem
     @SuppressLint("RestrictedApi", "NotifyDataSetChanged")
     private fun setupButtons(user : User) {
-        val addButton : TextView = view.findViewById(R.id.todo_add_button)
-        addButton.setOnClickListener{
-            val addItem : TextView = view.findViewById<TextView?>(R.id.todo_add_text)
-            val itemID = UUID.randomUUID().toString()
-            val item = TodoItem(itemID,"", false)
+        val addButton: TextView = view.findViewById(R.id.todo_add_button)
+        addButton.setOnClickListener {
+            val addItem: TextView = view.findViewById(R.id.todo_add_text)
 
-            item.task = addItem.text.toString()
-            userViewModel.addTodoItem(user.id,item)
+            if (TextUtils.isEmpty(addItem.text.toString())) {
+                addItem.hint = "Please do not leave this field blank"
+                addItem.setHintTextColor(Color.RED)
+            }
+            else {
+                val itemID = UUID.randomUUID().toString()
+                val item = TodoItem(itemID, addItem.text.toString(), false)
 
-            //Clear text field
-            addItem.text = ""
-            addItem.clearFocus()
-            todoListView.adapter?.notifyItemInserted(todoList.size)
+                userViewModel.addTodoItem(user.id, item)
 
-            //Close keyboard
-            val kb = requireActivity().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-            kb.hideSoftInputFromWindow(view.windowToken, 0)
+                //Clear text field
+                addItem.text = ""
+                addItem.hint = "Add a new todo item here"
+                addItem.setHintTextColor(Color.GRAY)
+                addItem.clearFocus()
+                todoListView.adapter?.notifyItemInserted(todoList.size)
 
-            Toast.makeText(requireContext(), "Success", Toast.LENGTH_SHORT).show()
+                //Close keyboard
+                val kb =
+                    requireActivity().getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+                kb.hideSoftInputFromWindow(view.windowToken, 0)
+
+                Toast.makeText(requireContext(), "Success", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
     //Initiate todoList
     private fun getUserTodoList(user : User) {
         todoList = user.todoList
-        todoListView.adapter = TodoRecyclerAdapter(todoList, userViewModel, user.id)
-    }
-
-    private fun observeDataStoreChanges() {
-        CoroutineScope(Dispatchers.IO).launch {
-            dataStoreManager.userIdFlow.collect { userId ->
-                println("ACCESS USERID HERE: $userId")
-                userViewModel.getUser(userId)
-            }
-        }
+        todoListAdapter = TodoRecyclerAdapter(userViewModel, user.id)
+        todoListAdapter.setTodoList(todoList)
+        todoListView.adapter = todoListAdapter
+        todoListAdapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/example/travelassistant/ui/todo/TodoRecyclerAdapter.kt
+++ app/src/main/java/com/example/travelassistant/ui/todo/TodoRecyclerAdapter.kt
@@ -14,20 +14,44 @@ import com.example.travelassistant.models.user.TodoItem
 import com.example.travelassistant.viewModels.UserViewModel
 
 
-class TodoRecyclerAdapter(items : List<TodoItem>, userModel : UserViewModel, userId : String) : RecyclerView.Adapter<TodoRecyclerAdapter.ViewHolder>() {
-    private var todoItemList = items
+class TodoRecyclerAdapter(userModel : UserViewModel, userId : String) : RecyclerView.Adapter<TodoRecyclerAdapter.ViewHolder>() {
+    private var todoItemList : ArrayList<TodoItem> = arrayListOf()
     private var myModel = userModel
     private var myUserId = userId
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val item : TextView = view.findViewById(R.id.textview_todo)
-        var finished : CheckBox = view.findViewById(R.id.checkbox_todo)
-        val delButton : Button = view.findViewById(R.id.delete_button)
+        val delButton : Button = view.findViewById<Button>(R.id.delete_button)
+
+        var finished : CheckBox = view.findViewById<CheckBox?>(R.id.checkbox_todo).apply {
+            setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    item.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
+                    todoItemList[adapterPosition].completed = true
+                    myModel.checkTodoItem(myUserId,todoItemList[adapterPosition].id)
+                    delButton.visibility = View.VISIBLE
+                }
+                else {
+                    item.paintFlags = Paint.ANTI_ALIAS_FLAG
+                    todoItemList[adapterPosition].completed = false
+                    myModel.unCheckTodoItem(myUserId,todoItemList[adapterPosition].id)
+                    delButton.visibility = View.INVISIBLE
+                }
+            }
+        }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.todo_item, parent,false)
+        val holder = ViewHolder(view)
 
-        return ViewHolder(view)
+        holder.delButton.setOnClickListener {
+            //todoItemList.removeAt(holder.adapterPosition)
+            myModel.deleteTodoItem(myUserId,todoItemList[holder.adapterPosition].id)
+            notifyItemRemoved(holder.adapterPosition)
+
+        }
+
+        return holder
     }
 
     //Adds strikethrough to text if checked
@@ -44,31 +68,11 @@ class TodoRecyclerAdapter(items : List<TodoItem>, userModel : UserViewModel, use
             holder.item.paintFlags = Paint.ANTI_ALIAS_FLAG
             holder.delButton.visibility = View.INVISIBLE
         }
-
-        //Delete todoItems
-        holder.finished.setOnCheckedChangeListener { _, isChecked : Boolean ->
-
-            if (isChecked) {
-                holder.item.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
-                myModel.checkTodoItem(myUserId,todoItemList[position].id)
-                holder.delButton.visibility = View.VISIBLE
-            }
-            else {
-                holder.item.paintFlags = Paint.ANTI_ALIAS_FLAG
-                myModel.unCheckTodoItem(myUserId,todoItemList[position].id)
-                holder.delButton.visibility = View.INVISIBLE
-            }
-        }
-
-        holder.delButton.setOnClickListener {
-            //todoFinishList.removeAt(holder.adapterPosition)
-            myModel.deleteTodoItem(myUserId,todoItemList[position].id)
-            //todoItemList.filterNot { it.id == todoItemList[position].id}
-            notifyItemRemoved(holder.adapterPosition)
-        }
     }
     override fun getItemCount(): Int {
         return todoItemList.size
     }
-
+    fun setTodoList(newList : List<TodoItem>) {
+        todoItemList = newList as ArrayList<TodoItem>
+    }
 }
