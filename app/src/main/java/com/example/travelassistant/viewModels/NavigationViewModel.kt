d7773113f906ac56cfa976ad0ad45d36b60edaed
Deniz Eren Evrendilek <devrendilek@gmail.com>
2023-12-04 01:08:57 -0800

    Delete NavigationViewModel.kt

--- app/src/main/java/com/example/travelassistant/viewModels/NavigationViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.travelassistant.viewModels
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class NavigationViewModel : ViewModel() {
-    val showCurrentUser = MutableLiveData(false)
-}
